54b55584f465f5ef29608c524dcfca1c1a49204b
Sivakkumar <sivakkmr21@gmail.com>
2024-02-03 18:50:33 +0530

    updated

--- myProject/src/app/app.component.ts
+++ myProject/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Tooltip, initTE } from 'tw-elements';
 
 
 @Component({
@@ -8,8 +7,6 @@ import { Tooltip, initTE } from 'tw-elements';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myProject';
-  ngOnInit() {
-    initTE({ Tooltip });
-  }
+  title = 'Matrimony-Project';
+  
 }
